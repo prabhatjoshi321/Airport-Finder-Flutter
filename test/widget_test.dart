227e3fd52bd96266225b34471145c352a77c880c
prabhatjoshi321 <prabhatjoshi321@users.noreply.github.com>
2020-02-26 02:12:17 +0530

    nn

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:first/main.dart';
 
-import 'package:airport_finder/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
